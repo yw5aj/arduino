d2fc72d58b84be1f6bddb84211cc9e82f6777c80
Yuxiang Wang <yw5aj@virginia.edu>
2015-04-16 21:39:49 -0400

    Significant digits

--- daq/ask_question.py
+++ daq/ask_question.py
@@ -1,30 +1,31 @@
 import serial
 import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    ser = serial.Serial('COM3', baudrate=1000000)
-    time0 = time.time()
+    ser = serial.Serial('COM3', baudrate=115200)
     f = open('data.csv', 'w')
     ser.readline()
-    while (time.time() - time0 < 10):
+    time0 = time.time()
+    for i in range(10000):
         try:
             data = int(ser.readline())
             current_time = time.time() - time0
-            f.write('%.8f, %d\n' % (current_time, data))
+            f.write('%.3f, %d\n' % (current_time, data))
             f.flush()
         except ValueError:
             pass
+    time1 = time.time()
     ser.close()
     f.close()
+    fs = 10000 / (time1 - time0)
     # %% Plot data
-    import matplotlib.pyplot as plt
-    import numpy as np
     time_array, data_array = np.loadtxt('data.csv', delimiter=',').T
     plt.plot(time_array, data_array)
     plt.ylim(-100, 1500)
     plt.xlabel('Time (s)')
     plt.ylabel('Data (0-1023)')
     plt.tight_layout()
-    plt.savefig('data_sensor_alone.png')
-#    plt.close()
+    plt.savefig('data.png')
